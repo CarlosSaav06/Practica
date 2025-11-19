7c19c63eb8fe94c284ae3ecdba11fa27257e9171
CarlosSaav06 <cfugarte@uamv.edu.ni>
2025-11-19 09:54:24 -0600

    primera parte del segundo ejercicio

---
+++ PracticaEjercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Creamos la lista y la llenamos con 30 números aleatorios
+        List<int> lista = new List<int>();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 30; i++)
+        {
+            lista.Add(rnd.Next(1, 201)); // números entre 1 y 200, para variedad
+        }
+    }
+}
