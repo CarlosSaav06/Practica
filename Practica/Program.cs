2b3b3e0418b8d036cc04e7ba27cf80103e34bab1
CarlosSaav06 <cfugarte@uamv.edu.ni>
2025-11-19 09:47:03 -0600

    segunda parte del codigo

--- Practica/Program.cs
+++ Practica/Program.cs
@@ -13,5 +13,30 @@ class Program
         {
             arreglo[i] = rnd.Next(1, 101); // del 1 al 100 para que no salgan números locos
         }
+
+        // Mostrando el arreglo generado solo para verificar qué salió
+        Console.WriteLine("Arreglo generado:");
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            Console.Write(arreglo[i] + " ");
+        }
+
+        // Pedimos el número que el usuario quiere buscar
+        Console.WriteLine("\n\nIngrese un número a buscar:");
+        int numeroBuscado = int.Parse(Console.ReadLine());
+
+        // Variable para guardar la posición si lo encontramos
+        int posicion = -1;
+
+        // Búsqueda lineal: básicamente recorrer uno por uno
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            // Si encontramos el número, anotamos la posición y salimos del ciclo
+            if (arreglo[i] == numeroBuscado)
+            {
+                posicion = i;
+                break;
+            }
+        }
     }
 }
