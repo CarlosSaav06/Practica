5593d72e2c7069b506df8789a9c40fec2b000b8a
CarlosSaav06 <cfugarte@uamv.edu.ni>
2025-11-19 09:40:57 -0600

    Add project files.

---
+++ Practica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Arreglo donde vamos a guardar los 20 números aleatorios
+        int[] arreglo = new int[20];
+        Random rnd = new Random();
+
+    }
+}
