36b10a4e6cce7625f91a44f024d3766cc2400a3a
CarlosSaav06 <cfugarte@uamv.edu.ni>
2025-11-24 09:46:25 -0600

    ejercicio terminado rapido debido a que los tenia en otra carpeta

---
+++ PracticaEjercicio6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticaEjercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
